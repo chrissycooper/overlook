06ef8561793437dcd38d54d7dbcc08db5ecc7ddd
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-03-02 17:34:07 -0600

    feat: Add API calls

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,18 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import { apiCalls } from './apiCalls';
+import Customer from './classes/Customer';
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+let testUser;
+
+Promise.all(apiCalls)
+    .then(values => {
+        const bookingsData = values[0].bookings
+        const roomsData = values[1].rooms
+        const customersData = values[2].customers
+        testUser = new Customer(values[3])
+        console.log(testUser)
+    })
