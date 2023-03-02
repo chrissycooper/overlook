0cdcac47997a78e45ed61c06fc8ec6084edc3f94
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-03-01 18:36:11 -0600

    test: Add basic booking class and test

--- src/classes/Booking.js
+++ src/classes/Booking.js
@@ -1,7 +1,14 @@
 class Booking {
-    constructor() {
+  constructor(user, date, room, id) {
+    this.id = id || Date.now();
+		this.userID = user.id;
+		this.date = date;
+		this.roomNumber = room.number;
+		//should this be a whoole room? How do we want to access the data about the room in the booking? we could find all rooms by room number and then access that data there
+  }
 
-    }
+	//if we want to create a new booking, we will probably need to pass in the id last
+    
 }
 
 export default Booking;
--- test/Booking-test.js
+++ test/Booking-test.js
@@ -1,8 +1,49 @@
 import chai from 'chai';
 const expect = chai.expect;
+import Booking from '../src/classes/Booking';
+import mockdata from '../src/data/mockdata';
+import Customer from '../src/classes/Customer';
+import Room from '../src/classes/Room';
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+describe('Booking', () => {
+  let booking1;
+  let booking2;
+  let user9;
+  let user1;
+  let room15;
+  let room12;
+
+  beforeEach(() => {
+    const bData1 = mockdata.bookings[0];
+    const bData2 = mockdata.bookings[mockdata.bookings.length-1];
+
+    user9 = new Customer(mockdata.customers[5]);
+    user1 = new Customer(mockdata.customers[0]);
+
+    room15 = new Room(mockdata.rooms[6]);
+    room12 = new Room(mockdata.rooms[5]);
+
+    booking1 = new Booking(user9, bData1.date, room15, bData1.id);
+    booking2 = new Booking(user1, bData2.date, room12, bData2.id);
+  })
+  
+  it('should have a customer', () => {
+    expect(booking1.userID).to.equal(9);
+    expect(booking2.userID).to.equal(1);
+  });
+  
+  it('should have a date', () => {
+    expect(booking1.date).to.equal("2022/04/22");
+    expect(booking2.date).to.equal("2022/02/05");
+  });
+
+  it('should have a room number', () => {
+    expect(booking1.roomNumber).to.equal(15);
+    expect(booking2.roomNumber).to.equal(12);
+  });
+  
+  it('should have an id', () => {
+    expect(booking1.id).to.equal("5fwrgu4i7k55hl6sz");
+    expect(booking2.id).to.equal("5fwrgu4i7k55hl6t8");
   });
 });
