72a0a0149c5338d93af099a921a3fbd1501376a8
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-03-03 17:39:00 -0600

    feat: Show list of available rooms by date

--- src/scripts.js
+++ src/scripts.js
@@ -5,34 +5,36 @@
 import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-import './images/motel-carpet.png'
+import './images/motel-carpet.png';
 import { apiCalls } from './apiCalls';
 import Customer from './classes/Customer';
-
-
-console.log('This is the JavaScript entry file - your code begins here.');
+import Hotel from './classes/Hotel';
 
 const userBookingsSection = document.getElementById("userBookings");
-const totalSpentHTML = document.getElementById("total-spent")
+const totalSpentHTML = document.getElementById("total-spent");
+const dateInput = document.getElementById("choiceOfDate");
+const submitButton = document.getElementById("dateSubmit");
+const availableRoomsDisplay = document.getElementById("availableBookings")
 
-let testUser, bookingsData, roomsData
+let outlookMotel, testUser;
 
 Promise.all(apiCalls)
     .then(values => {
-        bookingsData = values[0].bookings
-        roomsData = values[1].rooms
-        const customersData = values[2].customers
-        testUser = new Customer(values[3])
-        console.log(testUser)
-        displayUserInfo()
-    })
+        const bookingsData = values[0].bookings;
+        const roomsData = values[1].rooms;
+        const customersData = values[2].customers;
+        testUser = new Customer(values[3]);
+        testUser.createBookingArray(bookingsData, roomsData);
+        outlookMotel = new Hotel();
+        outlookMotel.parseHotelData(roomsData, bookingsData, customersData);
+        console.log('outlookmotel', outlookMotel);
+        displayUserInfo();
+    });
 
+submitButton.addEventListener('click', displayAvailableRooms);
 
 function displayUserInfo(){
-    testUser.createBookingArray(bookingsData, roomsData)
-    console.log(testUser.bookings)
-    userBookingsSection.innerHTML = ''
+    userBookingsSection.innerHTML = `<h2 class="yourBookings">Your Booking History</h2>`
     testUser.bookings.forEach(booking => {
         userBookingsSection.innerHTML += 
         `
@@ -42,10 +44,31 @@ function displayUserInfo(){
         <p>Cost Per Night: ${booking.costPerNight}</p>
         </div>
         `
-    
     totalSpentHTML.innerHTML = `$${testUser.calculateTotalSpent()}`
     })
+}
 
-
-    //sort them by date?
+function displayAvailableRooms(event) {
+    event.preventDefault();
+    if(dateInput.value) {
+        const convertedDate = dateInput.value.split('-').join('/');
+        const availableRooms = outlookMotel.filterForAvailableRooms(convertedDate)
+        console.log(availableRooms)
+        availableRoomsDisplay.innerHTML = '';
+        availableRooms.forEach(room => {
+            let answer = room.bidet ? 'Bidet!' : 'No Bidet :(';
+            availableRoomsDisplay.innerHTML +=
+                `
+                <div class="available-bookings dashboard">
+                    <h3 class="room-type">${room.type}</h3>
+                    <p>Date: ${dateInput.value}</p>
+                    <p>Room Number: ${room.number}</p>
+                    <p>Bed Size: ${room.bedSize}</p>
+                    <p>${room.numBeds} ${room.numBeds === 1 ? "Bed" : "Beds"}</p>
+                    <p>${answer}</p>
+                    <p>Cost Per Night: ${room.costPerNight}</p>
+                </div>
+            `
+        })
+    }
 }
