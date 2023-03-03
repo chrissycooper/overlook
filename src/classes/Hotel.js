84a949260c4628e811635157a259faffca5fe61a
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-03-03 15:03:29 -0600

    feat: Add filter by available rooms functionality for Hotel class

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -1,4 +1,4 @@
-import Room from "./Room"
+import Room from "./Room";
 import Booking from "./Booking";
 import Customer from "./Customer";
 
@@ -10,18 +10,32 @@ class Hotel {
   }
 
 	parseHotelData(roomsData, bookingsData, customerData) {
-		this.rooms = roomsData.map(room => new Room(room))
+		this.rooms = roomsData.map(room => new Room(room));
 		this.bookings = bookingsData.map(booking => {
-			const roomMatch = this.rooms.find(room => room.number === booking.roomNumber)
-			return new Booking(booking.userID, booking.date, roomMatch, booking.id)
-		})
-		this.customers = customerData.map(user => {
-			const customer = new Customer(user)
-			customer.createBookingArray(bookingsData, roomsData)
-			return customer
-		})
-	}
+			const roomMatch = this.rooms.find(room => room.number === booking.roomNumber);
+			return new Booking(booking.userID, booking.date, roomMatch, booking.id);
+		});
 
-}
+		this.customers = customerData.map(user => {
+			const customer = new Customer(user);
+			customer.createBookingArray(bookingsData, roomsData);
+			return customer;
+		});
+	};
+
+	filterForAvailableRooms(date) {
+		const roomsBooked = this.bookings.filter(booking => booking.date === date).map(booking => booking.roomNumber);
+		
+		const availableRooms = this.rooms.reduce((acc, room) => {
+			if(!roomsBooked.find(num => num === room.number)){
+				acc.push(room);
+			}
+			return acc;
+		}, []);
+
+		return availableRooms;
+	};
+
+};
 
 export default Hotel;
--- test/Hotel-test.js
+++ test/Hotel-test.js
@@ -8,41 +8,45 @@ describe('Hotel', () => {
   let hotel;
 
   beforeEach(() => {
-    hotel = new Hotel()
-  })
+    hotel = new Hotel();
+		hotel.parseHotelData(rooms, bookings, customers);
+  });
 
   it('Should have properties for rooms, bookings, and customers', () => {
-    expect(hotel.rooms).to.deep.equal([]);
-    expect(hotel.bookings).to.deep.equal([]);
-    expect(hotel.customers).to.deep.equal([]);
+    expect(hotel.rooms).to.be.an('array');
+    expect(hotel.bookings).to.be.an('array');
+    expect(hotel.customers).to.be.an('array');
   });
 
 	it('Should be able to parse room data', () => {
-		hotel.parseHotelData(rooms, bookings, customers)
-		expect(hotel.rooms.length).to.equal(8)
-		expect(hotel.rooms[0].type).to.equal("residential suite")
-		expect(hotel.rooms[0].bidet).to.equal(true)
-		expect(hotel.rooms[0].bedSize).to.equal("queen")
-		expect(hotel.rooms[0].number).to.equal(1)
-		expect(hotel.rooms[0].numBeds).to.equal(1)
-		expect(hotel.rooms[0].costPerNight).to.equal(358.4)
-	})
+		expect(hotel.rooms.length).to.equal(8);
+		expect(hotel.rooms[0].type).to.equal("residential suite");
+		expect(hotel.rooms[0].bidet).to.equal(true);
+		expect(hotel.rooms[0].bedSize).to.equal("queen");
+		expect(hotel.rooms[0].number).to.equal(1);
+		expect(hotel.rooms[0].numBeds).to.equal(1);
+		expect(hotel.rooms[0].costPerNight).to.equal(358.4);
+	});
 
 	it('Should be able to parse bookings data', () => {
-		hotel.parseHotelData(rooms, bookings, customers)
-		expect(hotel.bookings.length).to.equal(5)
-		expect(hotel.bookings[0].id).to.equal("5fwrgu4i7k55hl6sz")
-		expect(hotel.bookings[0].userID).to.equal(9)
-		expect(hotel.bookings[0].date).to.equal("2022/04/22")
-		expect(hotel.bookings[0].roomNumber).to.equal(15)
-	})
+		expect(hotel.bookings.length).to.equal(5);
+		expect(hotel.bookings[0].id).to.equal("5fwrgu4i7k55hl6sz");
+		expect(hotel.bookings[0].userID).to.equal(9);
+		expect(hotel.bookings[0].date).to.equal("2022/04/22");
+		expect(hotel.bookings[0].roomNumber).to.equal(15);
+	});
 
 	it('Should be able to parse customer data', () => {
-		hotel.parseHotelData(rooms, bookings, customers)
-		expect(hotel.customers.length).to.equal(9)
-		expect(hotel.customers[0].id).to.equal(1)
-		expect(hotel.customers[0].name).to.equal("Leatha Ullrich")
-		expect(hotel.customers[0].bookings[0].id).to.equal("5fwrgu4i7k55hl6t8")
-		expect(hotel.customers[0].bookings[0].costPerNight).to.equal(172.09)
-	})
+		expect(hotel.customers.length).to.equal(9);
+		expect(hotel.customers[0].id).to.equal(1);
+		expect(hotel.customers[0].name).to.equal("Leatha Ullrich");
+		expect(hotel.customers[0].bookings[0].id).to.equal("5fwrgu4i7k55hl6t8");
+		expect(hotel.customers[0].bookings[0].costPerNight).to.equal(172.09);
+	});
+
+	it('Should be able to filter for available rooms', () => {
+		const allRooms = hotel.rooms;
+		const availableRooms = hotel.filterForAvailableRooms("2022/04/22");
+    expect(availableRooms).to.be.an('array').that.does.not.include(allRooms[6]);
+  });
 });
