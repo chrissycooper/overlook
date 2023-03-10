1b934383304f671647195aee446f325712aa1ab6
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-03-10 16:32:12 -0600

    refactor: Delete unnecessary lines from GET requests

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,22 +4,18 @@ let apiCalls;
 
 const bookingsData = fetch("http://localhost:3001/api/v1/bookings")
     .then(response => response.json())
-    .then(data => data)
     .catch(err => console.log(err));
 
 const roomsData = fetch("http://localhost:3001/api/v1/rooms")
     .then(response => response.json())
-    .then(data => data)
     .catch(err => console.log(err));
 
 const customersData = fetch("http://localhost:3001/api/v1/customers")
     .then(response => response.json())
-    .then(data => data)
     .catch(err => console.log(err));
 
 const testCustomer = fetch("http://localhost:3001/api/v1/customers/6")
     .then(response => response.json())
-    .then(data => data)
     .catch(err => console.log(err));
 
 apiCalls = [bookingsData, roomsData, customersData, testCustomer];
